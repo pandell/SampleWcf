12d920815aa0d842942288847ec21033c4fbd3f1
Milan Gardian <mgardian@gmail.com>
2011-08-18 08:01:47 -0600

    Add a WCF web service 'Call.svc' with one endpoint, IFileTracker

---
+++ Source/SampleWcf.Web/Call.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ServiceModel.Activation;
+
+namespace SampleWcf.Web
+{
+
+    /// <summary>
+    /// </summary>
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Call : IFileTracker
+    {
+
+        //**************************************************
+        //* Interface IFileTracker implementation
+        //**************************************************
+
+        //--------------------------------------------------
+        /// <summary>
+        /// </summary>
+        public void Track(IEnumerable<string> files)
+        {
+            Debugger.Log(0, null, string.Concat("--- Received files:", Environment.NewLine));
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    Debugger.Log(0, null, string.Concat("File: \"", file, "\"", Environment.NewLine));
+                }
+            }
+            Debugger.Log(0, null, string.Concat("--- Received files end", Environment.NewLine));
+        }
+
+    }
+
+}
---
+++ Source/SampleWcf.Web/FileTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace SampleWcf.Web
+{
+
+    [ServiceContract]
+    public interface IFileTracker
+    {
+
+        [OperationContract]
+        void Track(IEnumerable<string> files);
+
+    }
+
+}
